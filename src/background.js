6b00ac77f471a117810cf59cf43183f0efd282a7
Osamu Ishimura <oishimura@outlook.com>
2019-06-25 21:06:40 +0900

    add tab counter on the icon

--- src/background.js
+++ src/background.js
@@ -1,107 +1,136 @@
 {
-	let nullPromise = function(){
-		return new Promise(function(resolve, reject){});
-	}
+    let nullPromise = function () {
+        return new Promise(function (resolve, reject) { });
+    }
 
-	let getTabsAsync = function(){
-		return new Promise(function(resolve, reject){chrome.tabs.query({}, resolve)});
-	}
+    let getTabsAsync = function () {
+        return new Promise(function (resolve, reject) { chrome.tabs.query({}, resolve) });
+    }
 
-	let moveTabAsync = function(tabids, winid){
-		return new Promise(function(resolve, reject){chrome.tabs.move(tabids, {index:-1,windowId:winid}, resolve)});
-	}
+    let moveTabAsync = function (tabids, winid) {
+        return new Promise(function (resolve, reject) { chrome.tabs.move(tabids, { index: -1, windowId: winid }, resolve) });
+    }
 
-	let removeTabAsync = function(tabid){
-		return new Promise(function(resolve, reject){chrome.tabs.remove(tabid, resolve)});
-	}
+    let removeTabAsync = function (tabid) {
+        return new Promise(function (resolve, reject) { chrome.tabs.remove(tabid, resolve) });
+    }
 
-	let reloadTabAsync = function(tabid){
-		return new Promise(function(resolve, reject){chrome.tabs.reload(tabid, resolve)});
-	}
+    let reloadTabAsync = function (tabid) {
+        return new Promise(function (resolve, reject) { chrome.tabs.reload(tabid, resolve) });
+    }
 
-	let getcWinAsync = function(){
-		return new Promise(function(resolve, reject){chrome.windows.getCurrent(resolve)});
-	}
+    let getcWinAsync = function () {
+        return new Promise(function (resolve, reject) { chrome.windows.getCurrent(resolve) });
+    }
 
-	var re = /^chrome:\/\//;
+    var re = /^chrome:\/\//;
 
-	let comclose = async function(flag){
-		if(!flag) return;
-		const tabs = await getTabsAsync();
-		let ps = tabs.filter(tab => re.test(tab.url))
-				           .map(el => removeTabAsync(el.id));
-		return Promise.all(ps);
-	}
+    let comclose = async function (flag) {
+        if (!flag) return;
+        const tabs = await getTabsAsync();
+        let ps = tabs.filter(tab => re.test(tab.url))
+            .map(el => removeTabAsync(el.id));
+        return Promise.all(ps);
+    }
 
-	let depclean = async function(flag){
-		if(!flag) return;
-		const tabs = await getTabsAsync();
-		let ps = tabs.filter((cTab, i) => tabs.slice(i + 1)
-		             .reduce((prev, tab) => cTab.url === tab.url || prev, false))
-		             .map(el => removeTabAsync(el.id));
-		return Promise.all(ps);
-	}
+    let depclean = async function (flag) {
+        if (!flag) return;
+        const tabs = await getTabsAsync();
+        let ps = tabs.filter((cTab, i) => tabs.slice(i + 1)
+            .reduce((prev, tab) => cTab.url === tab.url || prev, false))
+            .map(el => removeTabAsync(el.id));
+        return Promise.all(ps);
+    }
 
-	let glue = function(flag){
-		if(!flag) return nullPromise();
-		else return Promise.all([getcWinAsync(), getTabsAsync()])
-		.then(([cWin, tabs]) => moveTabAsync(tabs.map(el => el.id), cWin.id));
-	}
+    let glue = function (flag) {
+        if (!flag) return nullPromise();
+        else return Promise.all([getcWinAsync(), getTabsAsync()])
+            .then(([cWin, tabs]) => moveTabAsync(tabs.map(el => el.id), cWin.id));
+    }
 
-	let refresh = async function(flag){
-		if(!flag) return;
-		const tabs = await getTabsAsync();
-		let ps = tabs.filter(el => el.discarded)
-		             .map(el => reloadTabAsync(el.id));
-		return Promise.all(ps);
-	}
+    let refresh = async function (flag) {
+        if (!flag) return;
+        const tabs = await getTabsAsync();
+        let ps = tabs.filter(el => el.discarded)
+            .map(el => reloadTabAsync(el.id));
+        return Promise.all(ps);
+    }
 
-	let urlcmp = function(a, b){
-		a = a.url.toLowerCase();
-		b = b.url.toLowerCase();
-		if(a < b) return -1;
-		else if(a > b) return 1;
-		else return 0;
-	}
+    let iconbadge = async function (flag) {
+        if (flag) {
+            const tabs = await getTabsAsync();
+            chrome.browserAction.setBadgeText({ text: tabs.length.toString() });
+        } else {
+            chrome.browserAction.setBadgeText({ text: "" });
+        }
+        return;
+    }
 
-	let titlecmp = function(a, b){
-		a = a.title.toLowerCase();
-		b = b.title.toLowerCase();
-		if(a < b) return -1;
-		else if(a > b) return 1;
-		else return 0;
-	}
+    let urlcmp = function (a, b) {
+        a = a.url.toLowerCase();
+        b = b.url.toLowerCase();
+        if (a < b) return -1;
+        else if (a > b) return 1;
+        else return 0;
+    }
 
-	let strcmp = {'url': urlcmp, 'title': titlecmp}
+    let titlecmp = function (a, b) {
+        a = a.title.toLowerCase();
+        b = b.title.toLowerCase();
+        if (a < b) return -1;
+        else if (a > b) return 1;
+        else return 0;
+    }
 
-	let sorttab = async function(flag, sorttype){
-		if(!flag) return;
-		const tabs = await getTabsAsync();
-		let ps = tabs.sort(strcmp[sorttype])
-		             .map(el => moveTabAsync(el.id));
-		return Promise.all(ps);
-	}
+    let strcmp = { 'url': urlcmp, 'title': titlecmp }
 
-	let main = function(){
-		chrome.storage.sync.get({
-				glue: true,
-				comclose: false, 
-				depclean: true,
-				sorttab: true,
-				sortkey: 'url',
-				refresh: true,
-			}, function(items) {
-				var _ = nullPromise()
-				.then(glue(items.glue))
-				.then(refresh(items.refresh))
-				.then(sorttab(items.sorttab, items.sortkey))
-				.then(depclean(items.depclean))
-				.then(comclose(items.comclose))
-				.then(_=> _);
-		});
-	}
+    let sorttab = async function (flag, sorttype) {
+        if (!flag) return;
+        const tabs = await getTabsAsync();
+        let ps = tabs.sort(strcmp[sorttype])
+            .map(el => moveTabAsync(el.id));
+        return Promise.all(ps);
+    }
 
+    chrome.browserAction.onClicked.addListener(function () {
+        chrome.storage.sync.get({
+            glue: true,
+            comclose: false,
+            depclean: true,
+            sorttab: true,
+            sortkey: 'url',
+            refresh: true,
+            iconbadge: true,
+        }, function (items) {
+            var _ = nullPromise()
+                .then(glue(items.glue))
+                .then(refresh(items.refresh))
+                .then(sorttab(items.sorttab, items.sortkey))
+                .then(depclean(items.depclean))
+                .then(comclose(items.comclose))
+                .then(iconbadge(items.iconbadge))
+                .then(_ => _);
+        });
+    })
 
-	chrome.browserAction.onClicked.addListener(main);
+    chrome.tabs.onUpdated.addListener(function () {
+        chrome.storage.sync.get({
+            iconbadge: true,
+        }, function (items) {
+            var _ = nullPromise()
+                .then(iconbadge(items.iconbadge))
+                .then(_ => _);
+        });
+    })
+
+    chrome.tabs.onRemoved.addListener(function () {
+        chrome.storage.sync.get({
+            iconbadge: true,
+        }, function (items) {
+            var _ = nullPromise()
+                .then(iconbadge(items.iconbadge))
+                .then(_ => _);
+        });
+    })
 
 }
--- src/options.js
+++ src/options.js
@@ -1,74 +1,72 @@
-function localizeHtmlPage()
-{
-	var objects = document.getElementsByTagName('html');
-	for (var j = 0; j < objects.length; j++)
-	{
-		var obj = objects[j];
+function localizeHtmlPage() {
+    var objects = document.getElementsByTagName('html');
+    for (var j = 0; j < objects.length; j++) {
+        var obj = objects[j];
 
-		var valStrH = obj.innerHTML.toString();
-		var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function(match, v1)
-			{
-				return v1 ? chrome.i18n.getMessage(v1) : "";
-		});
+        var valStrH = obj.innerHTML.toString();
+        var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function (match, v1) {
+            return v1 ? chrome.i18n.getMessage(v1) : "";
+        });
 
-		if(valNewH != valStrH)
-		{
-			obj.innerHTML = valNewH;
-		}
-	}
+        if (valNewH != valStrH) {
+            obj.innerHTML = valNewH;
+        }
+    }
 }
 
-
-function fixrefreshoption(){
-	if(document.getElementById('sorttab').checked && document.getElementById('sortkey').value=='title') {
-		document.getElementById('refresh').checked = true;
-		document.getElementById('refresh').disabled = true;
-	}else{
-		document.getElementById('refresh').disabled = false;
-	}
+function fixrefreshoption() {
+    if (document.getElementById('sorttab').checked && document.getElementById('sortkey').value == 'title') {
+        document.getElementById('refresh').checked = true;
+        document.getElementById('refresh').disabled = true;
+    } else {
+        document.getElementById('refresh').disabled = false;
+    }
 }
 
 function save_options() {
-	fixrefreshoption();
-	var glue = document.getElementById('glue').checked;
-	var depclean = document.getElementById('depclean').checked;
-	var sorttab = document.getElementById('sorttab').checked;
-	var sortkey = document.getElementById('sortkey').value;
-	var refresh = document.getElementById('refresh').checked;
-	var comclose = document.getElementById('comclose').checked;
+    fixrefreshoption();
+    var glue = document.getElementById('glue').checked;
+    var depclean = document.getElementById('depclean').checked;
+    var sorttab = document.getElementById('sorttab').checked;
+    var sortkey = document.getElementById('sortkey').value;
+    var refresh = document.getElementById('refresh').checked;
+    var comclose = document.getElementById('comclose').checked;
+    var iconbadge = document.getElementById('iconbadge').checked;
 
-	chrome.storage.sync.set({
-			glue: glue,
-			depclean: depclean,
-			sorttab: sorttab,
-			sortkey: sortkey,
-			refresh: refresh,
-			comclose: comclose
-		});
+    chrome.storage.sync.set({
+        glue: glue,
+        depclean: depclean,
+        sorttab: sorttab,
+        sortkey: sortkey,
+        refresh: refresh,
+        comclose: comclose,
+        iconbadge: iconbadge
+    });
 }
-
 
 function restore_options() {
-	chrome.storage.sync.get({
-			glue: true,
-			depclean: true,
-			sorttab: true,
-			sortkey: 'url',
-			refresh: true,
-			comclose: false
-		}, function(items) {
-			document.getElementById('glue').checked = items.glue;
-			document.getElementById('depclean').checked = items.depclean;
-			document.getElementById('sorttab').checked = items.sorttab;
-			document.getElementById('sortkey').value = items.sortkey;
-			document.getElementById('refresh').checked = items.refresh;
-			document.getElementById('comclose').checked = items.comclose;
-			fixrefreshoption();
-	});
+    chrome.storage.sync.get({
+        glue: true,
+        depclean: true,
+        sorttab: true,
+        sortkey: 'url',
+        refresh: true,
+        comclose: false,
+        iconbadge: false,
+    }, function (items) {
+        document.getElementById('glue').checked = items.glue;
+        document.getElementById('depclean').checked = items.depclean;
+        document.getElementById('sorttab').checked = items.sorttab;
+        document.getElementById('sortkey').value = items.sortkey;
+        document.getElementById('refresh').checked = items.refresh;
+        document.getElementById('comclose').checked = items.comclose;
+        document.getElementById('iconbadge').checked = items.iconbadge;
+        fixrefreshoption();
+    });
 }
 
-document.addEventListener('DOMContentLoaded',()=>{
-		restore_options();
-		localizeHtmlPage();
-		document.getElementById('form').addEventListener('change', save_options);
+document.addEventListener('DOMContentLoaded', () => {
+    restore_options();
+    localizeHtmlPage();
+    document.getElementById('form').addEventListener('change', save_options);
 });
